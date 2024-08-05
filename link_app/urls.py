82ffb1bcc178b928e4fba8e3f4f751a9992f16cc
stac10 <michalkobylarz@gmail.com>
2024-08-05 21:27:00 +0200

    view html przekierowanie link_app nazwą

--- link_app/urls.py
+++ link_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from link_app import views
 
+
+app_name = 'link_app'
 urlpatterns = [
     path('first/', views.first_view, name='first'),
     path('second/', views.second_view, name='second'),
